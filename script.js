566fda43341304ced45fb9ba3d2f80253eadca4b
kaylane sophia <kaylanesophia220@gmail.com>
2024-06-21 22:13:30 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+  
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+  //substituir a imagem
+  if(html.classList.contains("light")){
+    //se tiver light mode, adicionar a imagem
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    //se tuiver sem o ligth mode, manter a imagem normal 
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+
+  //if(html.classList.contains("light")){
+  //  html.classList.remove("light")
+  //} else {
+  //  html.classList.add("light")
+  //}
+
+
+}
+
