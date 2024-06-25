00bb95c4baafc823d22bbe11bbf6b12a530851f2
kaylane sophia <kaylanesophia220@gmail.com>
2024-06-25 20:38:06 -0300

    correcting the js path

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle('light')
-  
   //pegar a tag img
   const img = document.querySelector("#profile img")
   //substituir a imagem
